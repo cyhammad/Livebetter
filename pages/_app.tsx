9c51a35310890ac5019e00e924300e1ef438025f
Adam Drago <atdrago@gmail.com>
2022-05-30 17:07:47 -0400

    fix: Prevent zoom when focusing on input fields

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
@@ -9,6 +10,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
+        </Head>
         <Component {...pageProps} />
       </Hydrate>
     </QueryClientProvider>
