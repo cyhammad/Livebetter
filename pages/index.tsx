ec878546ab5232a3e4ea37cbbf57c5dd9445b986
Adam Drago <atdrago@gmail.com>
2022-05-30 17:07:27 -0400

    feat: Add magnifying glass icon to search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import classNames from "classnames";
 import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
-import { CrosshairSimple, Sliders, Spinner, X } from "phosphor-react";
+import {
+  CrosshairSimple,
+  MagnifyingGlass,
+  Sliders,
+  Spinner,
+  X,
+} from "phosphor-react";
 import {
   ChangeEvent,
   useDeferredValue,
@@ -161,20 +167,30 @@ const Home: NextPage<HomeProps> = () => {
               style={{ gridTemplateColumns: "auto 1fr 1fr" }}
             >
               <h2 className="text-3xl sm:text-4xl font-bold">Restaurants</h2>
-              <input
-                type="search"
-                className="
-                  text-sm md:text-base
-                  mt-0 px-0.5 mx-0.5
-                  border-0 border-b border-slate-300
-                  focus:ring-0 focus:border-black
-                  row-start-2 md:row-start-1 md:col-start-2 col-span-3 md:col-span-1
-                "
-                value={searchTerm}
-                onChange={(event) => {
-                  setSearchTerm(event.target.value);
-                }}
-              />
+              <div className="grid row-start-2 md:row-start-1 md:col-start-2 col-span-3 md:col-span-1 items-center">
+                <input
+                  type="search"
+                  className="
+                    w-full
+                    text-sm md:text-base
+                    mt-0 px-0.5 mx-0.5 pl-6
+                    border-0 border-b border-slate-300
+                    focus:ring-0 focus:border-black
+                  "
+                  value={searchTerm}
+                  onChange={(event) => {
+                    setSearchTerm(event.target.value);
+                  }}
+                  style={{ gridArea: "1 / 1" }}
+                />
+                <MagnifyingGlass
+                  color="currentColor"
+                  className="h-5 w-5 text-gray-300"
+                  size={20}
+                  style={{ gridArea: "1 / 1" }}
+                  weight="bold"
+                />
+              </div>
               <div className="flex gap-2 items-center ml-auto col-start-3">
                 <Spinner
                   size={32}
