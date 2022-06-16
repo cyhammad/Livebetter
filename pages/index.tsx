24dfffcbd233438c24477c7c85aae109c80fde3f
Adam Drago <atdrago@gmail.com>
2022-06-16 17:26:05 -0400

    fix(home): Sort sections correctly

--- pages/index.tsx
+++ pages/index.tsx
@@ -124,66 +124,70 @@ const Home: NextPage<HomeProps> = () => {
         </Toolbar>
         <div ref={restaurantListTopRef}></div>
         <div className="flex flex-col col-span-2 gap-8 h-full w-full">
-          {Object.entries(data?.sections ?? {}).map(
-            ([sectionKey, restaurants]) => {
-              return (
-                <section className="flex flex-col gap-2" key={sectionKey}>
-                  <h3 className="text-xl sm:text-2xl font-bold container mx-auto ">
-                    <div className="px-4 sm:px-6">
-                      {sectionKeyToHeadingMap[sectionKey as FeaturedSection]}
-                    </div>
-                  </h3>
-                  <ul
-                    className="flex overflow-auto gap-4 sm:gap-8 pl-4 sm:pl-0
-                    snap-x snap-mandatory scroll-ml-4 scroll-pl-4
-                    sm:scroll-pl-[calc(((100vw-640px)/2)+1.5rem)]
-                    sm:scroll-ml-[calc(((100vw-640px)/2)+1.5rem)]
-                    md:scroll-pl-[calc(((100vw-768px)/2)+1.5rem)]
-                    md:scroll-ml-[calc(((100vw-768px)/2)+1.5rem)]
-                    lg:scroll-pl-[calc(((100vw-1024px)/2)+1.5rem)]
-                    lg:scroll-ml-[calc(((100vw-1024px)/2)+1.5rem)]
-                    xl:scroll-pl-[calc(((100vw-1280px)/2)+1.5rem)]
-                    xl:scroll-ml-[calc(((100vw-1280px)/2)+1.5rem)]
-                    2xl:scroll-pl-[calc(((100vw-1536px)/2)+1.5rem)]
-                    2xl:scroll-ml-[calc(((100vw-1536px)/2)+1.5rem)]
-                  "
-                  >
-                    {restaurants.map((restaurant, index) => (
-                      <li
-                        className={classNames(
-                          "flex flex-col flex-none gap-5 snap-start",
-                          index === 0
-                            ? [
-                                "sm:pl-[calc(((100vw-640px)/2)+1.5rem)]",
-                                "md:pl-[calc(((100vw-768px)/2)+1.5rem)]",
-                                "lg:pl-[calc(((100vw-1024px)/2)+1.5rem)]",
-                                "xl:pl-[calc(((100vw-1280px)/2)+1.5rem)]",
-                                "2xl:pl-[calc(((100vw-1536px)/2)+1.5rem)]",
-                              ]
-                            : index === restaurants.length - 1
-                            ? [
-                                "pr-4",
-                                "sm:pr-[calc(((100vw-640px)/2)+1.5rem)]",
-                                "md:pr-[calc(((100vw-768px)/2)+1.5rem)]",
-                                "lg:pr-[calc(((100vw-1024px)/2)+1.5rem)]",
-                                "xl:pr-[calc(((100vw-1280px)/2)+1.5rem)]",
-                                "2xl:pr-[calc(((100vw-1536px)/2)+1.5rem)]",
-                              ]
-                            : []
-                        )}
-                        key={`${restaurant.Restaurant}${index}`}
-                      >
-                        <RestaurantCard
-                          layout="vertical"
-                          restaurant={restaurant}
-                        />
-                      </li>
-                    ))}
-                  </ul>
-                </section>
-              );
+          {getSectionKeys().map((sectionKey) => {
+            const restaurants = data?.sections[sectionKey];
+
+            if (!restaurants) {
+              return null;
             }
-          )}
+
+            return (
+              <section className="flex flex-col gap-2" key={sectionKey}>
+                <h3 className="text-xl sm:text-2xl font-bold container mx-auto ">
+                  <div className="px-4 sm:px-6">
+                    {sectionKeyToHeadingMap[sectionKey as FeaturedSection]}
+                  </div>
+                </h3>
+                <ul
+                  className="flex overflow-auto gap-4 sm:gap-8 pl-4 sm:pl-0
+                  snap-x snap-mandatory scroll-ml-4 scroll-pl-4
+                  sm:scroll-pl-[calc(((100vw-640px)/2)+1.5rem)]
+                  sm:scroll-ml-[calc(((100vw-640px)/2)+1.5rem)]
+                  md:scroll-pl-[calc(((100vw-768px)/2)+1.5rem)]
+                  md:scroll-ml-[calc(((100vw-768px)/2)+1.5rem)]
+                  lg:scroll-pl-[calc(((100vw-1024px)/2)+1.5rem)]
+                  lg:scroll-ml-[calc(((100vw-1024px)/2)+1.5rem)]
+                  xl:scroll-pl-[calc(((100vw-1280px)/2)+1.5rem)]
+                  xl:scroll-ml-[calc(((100vw-1280px)/2)+1.5rem)]
+                  2xl:scroll-pl-[calc(((100vw-1536px)/2)+1.5rem)]
+                  2xl:scroll-ml-[calc(((100vw-1536px)/2)+1.5rem)]
+                "
+                >
+                  {restaurants.map((restaurant, index) => (
+                    <li
+                      className={classNames(
+                        "flex flex-col flex-none gap-5 snap-start",
+                        index === 0
+                          ? [
+                              "sm:pl-[calc(((100vw-640px)/2)+1.5rem)]",
+                              "md:pl-[calc(((100vw-768px)/2)+1.5rem)]",
+                              "lg:pl-[calc(((100vw-1024px)/2)+1.5rem)]",
+                              "xl:pl-[calc(((100vw-1280px)/2)+1.5rem)]",
+                              "2xl:pl-[calc(((100vw-1536px)/2)+1.5rem)]",
+                            ]
+                          : index === restaurants.length - 1
+                          ? [
+                              "pr-4",
+                              "sm:pr-[calc(((100vw-640px)/2)+1.5rem)]",
+                              "md:pr-[calc(((100vw-768px)/2)+1.5rem)]",
+                              "lg:pr-[calc(((100vw-1024px)/2)+1.5rem)]",
+                              "xl:pr-[calc(((100vw-1280px)/2)+1.5rem)]",
+                              "2xl:pr-[calc(((100vw-1536px)/2)+1.5rem)]",
+                            ]
+                          : []
+                      )}
+                      key={`${restaurant.Restaurant}${index}`}
+                    >
+                      <RestaurantCard
+                        layout="vertical"
+                        restaurant={restaurant}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </section>
+            );
+          })}
         </div>
       </main>
     </>
