759bf99044cc45ad77e04019cb4c6e2148869325
Adam Drago <atdrago@gmail.com>
2022-06-16 16:30:24 -0400

    feat: Add current location to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,18 @@
+import classNames from "classnames";
 import Image from "next/image";
 import Link from "next/link";
+import { MapPin } from "phosphor-react";
 import { forwardRef } from "react";
+
+import { useUserContext } from "hooks/useUserContext";
 
 export const HEADER_HEIGHT = 48;
 
 export const Header = forwardRef<HTMLElement>(function Header(_, ref) {
+  const { location } = useUserContext();
+
+  const address = location?.address.split(",")[0];
+
   return (
     <header
       ref={ref}
@@ -12,7 +20,7 @@ export const Header = forwardRef<HTMLElement>(function Header(_, ref) {
     >
       <div className="container mx-auto flex justify-between items-center py-2 px-4 sm:px-6">
         <Link href="/">
-          <a className="flex gap-3 items-center container mx-auto">
+          <a className="flex gap-3 items-center">
             <div className="flex-none">
               <Image
                 alt="Live Better logo"
@@ -27,6 +35,23 @@ export const Header = forwardRef<HTMLElement>(function Header(_, ref) {
             <h1 className="text-2xl font-bold">Live Better</h1>
           </a>
         </Link>
+        {address ? (
+          <span className="flex gap-1 items-center text-sm">
+            <MapPin
+              size={16}
+              color="currentColor"
+              fill="currentColor"
+              weight={location ? "duotone" : "light"}
+              className={classNames({
+                // "text-black": !location,
+                "text-emerald-600 fill-emerald-600": true,
+                // "h-6 w-6 sm:h-7 sm:w-7": true,
+              })}
+            />
+            {address}
+          </span>
+        ) : null}
+        {/* <span></span> */}
       </div>
     </header>
   );
