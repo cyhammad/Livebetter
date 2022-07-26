8d051ed63c2bf44570ad839d48fbf6043ba59223
Adam Drago <atdrago@gmail.com>
2022-07-25 22:01:06 -0400

    fix: Remove "New Website Coming Soon" banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,17 @@
 import classNames from "classnames";
 import Image from "next/future/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { MapPin } from "phosphor-react";
 import { forwardRef } from "react";
 
 import { useUserContext } from "hooks/useUserContext";
-import { legacyRestaurantNameToUrlParam } from "lib/restaurantNameToUrlParam";
 
 export const HEADER_HEIGHT = 48;
-export const WARNING_HEIGHT = 44;
 
 export const Header = forwardRef<HTMLElement>(function Header(_, ref) {
   const { location } = useUserContext();
-  const router = useRouter();
 
   const address = location?.address.split(",")[0];
-
-  const legacyPath =
-    typeof router.query.restaurantName === "string"
-      ? router.pathname.replace(
-          "[restaurantName]",
-          legacyRestaurantNameToUrlParam(router.query.restaurantName)
-        )
-      : router.asPath;
 
   return (
     <header
@@ -62,29 +50,6 @@ export const Header = forwardRef<HTMLElement>(function Header(_, ref) {
             />
             {address}
           </span>
-        ) : null}
-      </div>
-      <div className="flex flex-col justify-center h-11 flex-shrink flex-grow-0 bg-amber-600 text-white px-3 py-1">
-        <p className="text-sm font-bold text-center">
-          New Website Coming Soon! 🎉
-        </p>
-        {typeof router.query.restaurantName === "string" ? (
-          <p className="text-xs text-center">
-            To order from{" "}
-            <span className="capitalize">
-              {router.query.restaurantName.toLocaleLowerCase()}
-            </span>
-            , please{" "}
-            <a
-              className="underline"
-              href={`https://livebetterphl.web.app${legacyPath}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              click here
-            </a>
-            .
-          </p>
         ) : null}
       </div>
     </header>
