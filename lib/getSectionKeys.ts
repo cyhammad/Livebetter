5e389e4d7aaffc63c77701454cb9dec659845906
Adam Drago <atdrago@gmail.com>
2022-07-28 06:49:30 -0400

    fix(home): Show featured sections based on time of day and week
    Resolves #72

--- lib/getSectionKeys.ts
+++ lib/getSectionKeys.ts
@@ -1,5 +1,28 @@
 import type { FeaturedSection } from "types";
 
 export const getSectionKeys = (): FeaturedSection[] => {
-  return ["tracking", "city_favorites", "dinner", "late_night"];
+  const now = new Date();
+  const hours = now.getHours();
+  const day = now.getDay();
+
+  const sectionKeys: FeaturedSection[] = ["tracking"];
+
+  // 6 = Saturday, 0 = Sunday
+  if ((day === 6 || day === 0) && hours >= 4 && hours <= 12 + 2) {
+    sectionKeys.push("brunch");
+  }
+
+  if (hours >= 4 && hours <= 10) {
+    sectionKeys.push("breakfast");
+  } else if (hours >= 11 && hours <= 12 + 3) {
+    sectionKeys.push("lunch");
+  } else if (hours >= 12 + 4 && hours <= 12 + 9) {
+    sectionKeys.push("dinner");
+  } else if (hours >= 12 + 10 || hours <= 3) {
+    sectionKeys.push("late_night");
+  }
+
+  sectionKeys.push("city_favorites");
+
+  return sectionKeys;
 };
