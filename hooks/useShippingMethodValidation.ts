eaa46a5a438fc677d9d8cc4b30aecdf18443039d
Adam Drago <atdrago@gmail.com>
2022-08-05 10:45:16 -0400

    fix(cart): Validate that the customer is within Pennsylvania

--- hooks/useShippingMethodValidation.ts
+++ hooks/useShippingMethodValidation.ts
@@ -15,7 +15,7 @@ export const useShippingMethodValidation = (
     isPickUpAvailable ? "pickup" : null,
   ].filter(notNullOrUndefined);
 
-  const { getDistanceToCoordinates } = useUserContext();
+  const { getDistanceToCoordinates, location } = useUserContext();
   const [shouldShowShippingMethodOptions, setShouldShowShippingMethodOptions] =
     useState(
       !shippingMethod || !allowedShippingMethods.includes(shippingMethod)
@@ -47,6 +47,18 @@ export const useShippingMethodValidation = (
         return [false, "Please enter your delivery address.", 1];
       }
 
+      const isPennsylvaniaAddress =
+        location?.address.includes(" PA ") &&
+        !location?.address.includes(" NJ ");
+
+      if (!isPennsylvaniaAddress) {
+        return [
+          false,
+          "Currently, we can only deliver within Pennsylvania.",
+          distance,
+        ];
+      }
+
       const isDeliveryWithinRange = !!(distance && distance <= 4);
 
       if (!isDeliveryWithinRange) {
@@ -59,7 +71,13 @@ export const useShippingMethodValidation = (
     }
 
     return [true, null, distance];
-  }, [getDistanceToCoordinates, Latitude, Longitude, shippingMethod]);
+  }, [
+    getDistanceToCoordinates,
+    Latitude,
+    Longitude,
+    shippingMethod,
+    location?.address,
+  ]);
 
   return {
     allowedShippingMethods,
