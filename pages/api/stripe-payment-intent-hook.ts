d6950b2bf39b537a4c1e334b983f917c52f54a84
Adam Drago <atdrago@gmail.com>
2022-08-09 08:11:13 -0400

    chore: Only send real orders to livebetterphl@gmail.com

--- pages/api/stripe-payment-intent-hook.ts
+++ pages/api/stripe-payment-intent-hook.ts
@@ -332,7 +332,7 @@ async function handler(
             await sendGridMail.send({
               from: "livebetterphl@gmail.com",
               to:
-                process.env.VERCEL_ENV === "development"
+                process.env.VERCEL_ENV !== "production"
                   ? "atdrago@gmail.com"
                   : "livebetterphl@gmail.com",
               subject: `New Order Notification ✔ (Order #${orderDoc.id})`,
