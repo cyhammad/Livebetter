ba5043ea4e8e08894fd2a65909b94bfbe26afe25
Adam Drago <atdrago@gmail.com>
2022-09-06 09:49:27 -0400

    fix(HorizontalStackWithSeparator): Add missing keys and simplify logic

--- components/HorizontalStackWithSeparator.tsx
+++ components/HorizontalStackWithSeparator.tsx
@@ -1,3 +1,5 @@
+import React, { Fragment } from "react";
+
 interface HorizontalStackWithSeparatorProps {
   separator: React.ReactNode;
   parts: React.ReactNode[];
@@ -9,11 +11,19 @@ export const HorizontalStackWithSeparator = ({
 }: HorizontalStackWithSeparatorProps) => {
   return (
     <>
-      {parts
-        .filter((part) => !!part)
-        .flatMap((part, index) =>
-          part ? (index !== 0 ? [separator, part] : part) : []
-        )}
+      {parts.reduce((acc: React.ReactNode[], part, index) => {
+        if (part) {
+          if (acc.length !== 0) {
+            acc.push(
+              <Fragment key={`${index}-separator`}>{separator}</Fragment>
+            );
+          }
+
+          acc.push(<Fragment key={`${index}-part`}>{part}</Fragment>);
+        }
+
+        return acc;
+      }, [])}
     </>
   );
 };
